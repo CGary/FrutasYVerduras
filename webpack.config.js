013768a2d6daf163831dd7309653826c696ec061
Cristian Pheno Gael <cristiangnj@gmail.com>
2020-10-19 07:08:16 -0400

    fix bugs webapck 5

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = (env, { mode }) => {
     //output
     output = {
       path: path.resolve(__dirname, "dist"),
-      filename: "js/[name].[hash].js",
+      filename: "js/[name].[contenthash].js",
       chunkFilename: "js/[id].[chunkhash].js",
     };
 
@@ -66,8 +66,8 @@ module.exports = (env, { mode }) => {
       new CompressionPlugin(),
       new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
-        filename: "css/[name].[hash].css",
-        chunkFilename: "css/[id].[hash].css",
+        filename: "css/[name].[contenthash].css",
+        chunkFilename: "css/[id].[contenthash].css",
       }),
       new OptimizeCSSAssetsPlugin(),
     ];
@@ -77,7 +77,7 @@ module.exports = (env, { mode }) => {
     optimization = {
       ...optimization,
       minimizer: [new TesterPlugin()],
-      namedChunks: true,
+      chunkIds: "named",
       splitChunks: {
         automaticNameDelimiter: ".",
         cacheGroups: {
@@ -112,7 +112,7 @@ module.exports = (env, { mode }) => {
 
     //rules
     //file-loader
-    rules[1].use.options.name = "[name].[hash].[ext]";
+    rules[1].use.options.name = "[name].[contenthash].[ext]";
     //css-loader
     rules[2].use = [
       {
